ac64e2bb63904bc502c1d169fe3dc4f5f3a5ec31
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-11-13 20:02:09 +0200

    fixing compiling problem

--- src/components/WinScore/WinScore.jsx
+++ src/components/WinScore/WinScore.jsx
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import scss from './WinScore.module.scss'
 import { selectScore } from 'redux/selectors'
 
