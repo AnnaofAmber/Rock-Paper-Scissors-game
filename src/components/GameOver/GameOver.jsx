05b406ffd7419956ffe27d55da5b585f3f10ddde
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-11-13 19:26:13 +0200

    adding play again

--- src/components/GameOver/GameOver.jsx
+++ src/components/GameOver/GameOver.jsx
@@ -1,16 +1,26 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import scss from './GameOver.module.scss'
 import { selectStatus } from 'redux/selectors'
+import { setAction } from 'redux/actionSlice'
+import { setPickedAction } from 'redux/pickedActionSlice'
+import { setStatus } from 'redux/statusSlice'
 
 export const GameOver = () => {
+const dispatch = useDispatch()
 const status = useSelector(selectStatus)
+
+const handleClick = () =>{
+dispatch(setAction('unselected'))
+dispatch(setPickedAction('unselected'))
+dispatch(setStatus('unselected'))
+}
 
     return (
         <div className={scss.container}>
             {status=== 'win' && <p className={scss.text}>You win</p>}
             {status=== 'lose' && <p className={scss.text}>You lose</p>}
             {status=== 'draw' && <p className={scss.text}> It's a draw</p>}
-            <button className={scss.btn}>play again</button>
+            <button className={scss.btn} onClick={handleClick}>play again</button>
         </div>
     )
 }
