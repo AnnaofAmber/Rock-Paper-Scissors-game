a914e45e4f9a53a6cc34a848a3e687a2accc63f2
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-11-21 18:46:14 +0200

    fixing modal

--- src/components/ModalRules/ModalRules.jsx
+++ src/components/ModalRules/ModalRules.jsx
@@ -10,27 +10,35 @@ export const ModalRules = () => {
     else{
       document.body.style.overflow = 'scroll';
     }
+    window.addEventListener('keydown', handleCloseClick);
+
+    return () => {
+      window.removeEventListener('keydown', handleCloseClick);
+    };
   },[isModal])
 
   const handleModalClick = () => {
     setIsModal(true);
 
   };
-  const handleCloseClisk = () => {
-  setIsModal(false)
+  const handleCloseClick = e => {
+    
+    if (e.target.nodeName === 'DIV' || e.code === 'Escape' || e.target.nodeName === 'BUTTON') {
+      setIsModal(false)}
   }
+
 
   return (
     <div className={scss.container}>
       <button onClick={handleModalClick} className={scss['btn-open']}>Rules</button>
       {isModal && (
-        <div className={scss.overlay}>
+        <div className={scss.overlay} onClick={handleCloseClick}>
           <div className={scss.modal}>
             <div className={scss['modal-container']}>
               {' '}
               <h2 className={scss.title}>rules</h2>
               <div className={scss.rules}></div>
-              <button onClick={handleCloseClisk} className={scss['btn-close']}></button>
+              <button onClick={handleCloseClick} className={scss['btn-close']}></button>
             </div>
           </div>
         </div>
